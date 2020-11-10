f77f6d6b5288b7f3974e34ef1676c107dd8cbec4
git config --global user.name <vlasovnew@yandex.ru>
2020-11-10 21:46:45 +0300

    add user navigation

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,18 +1,35 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">Мой блог</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-       <!-- <div class="collapse navbar-collapse" id="navbarResponsive">
+        @guest
+
+        @endguest
+        <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        Обо мне
-                    </a>
-                </li>
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href={{ route('loginForm') }}>
+                            Вход
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href={{ route('registerForm') }}>
+                            Регистрация
+                        </a>
+                    </li>
+                @endguest
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href={{ route('logout') }}>
+                            Выход
+                        </a>
+                    </li>
+                @endauth
             </ul>
         </div>
-        -->
     </div>
 </nav>
