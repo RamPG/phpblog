327f746975f22961f159336c007082852bb73a78
RamPG <vlasovnew@yandex.ru>
2021-01-30 13:30:03 +0000

    test server

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/login', function (Request $request) {
+    $user = \App\Models\User::where('email', $request->input('email'))->first();
+    return response()->json(['token' => $user->createToken('authToken')]);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
